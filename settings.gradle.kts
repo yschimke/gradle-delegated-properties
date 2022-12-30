edabbd7097e884fbf203f73fb57070ddff015acc
Yuri Schimke <yuri@schimke.ee>
2022-12-30 09:19:31 +0000

    Repro delegated properties failure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
 rootProject.name = "reproducer-project"
+
+val anInt: Int by settings
+val aBoolean: Boolean by settings
+val aString: String by settings
+
+println("Safe so far")
+
+println(aString)
+
+println("Still Safe")
+
+println(aBoolean)
